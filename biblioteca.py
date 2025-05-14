ae527bd91078473a7b7ef977a7c2449019fa7189
Felixrds77 <felixcanarinho@gmail.com>
2025-05-14 09:34:10 -0300

    Animais

--- biblioteca.py
+++ biblioteca.py
@@ -1,6 +1,42 @@
-class Animal():
-    def init (self,nome,cor):
-        super(). __init__(nome,cor)
+class Animal:
+    def __init__(self,nome,cor,comida):
+        self.nome = nome
+        self.cor = cor
+        self.comida = comida
+    def comer(self):
+        print(f"o {self.nome} foi comer...")
+class Gato(Animal):
+    def init (self,nome,cor,comida):
+        super(). __init__(nome,cor,comida)
 
     def miar(self):
-        print(f"o {self.nome} foi miando")
+        print(f"o {self.nome} foi miando...")
+    def comer(self):
+            print(f"o {self.nome} está comendo {self.comida}")
+
+class Cachorro(Animal):
+    def init(self,nome,cor,comida):
+        super(). __init__(nome,cor,comida)
+
+    def latir(self):
+        print(f"o {self.nome} estava latindo pra pegar a canela do motoboy")
+    def comer(self):
+        print(f"o {self.nome} está comendo {self.comida}")
+
+class Vaca(Animal):
+    def init(self,nome,cor,comida):
+        super(). __init__(nome,cor,comida)
+
+    def mugir(self):
+        print(f"A {self.nome} está mugindo para seu filhote")
+    def comer(self):
+        print(f"o {self.nome} está comendo {self.comida}")
+
+class Coelho(Animal):
+    def init(self,nome,cor,comida):
+        super(). __init__(nome,cor,comida)
+
+    def pular(self):
+        print(f"o {self.nome} estava pulando")
+    def comer(self):
+        print(f"o {self.nome} está comendo {self.comida}")
--- exercicio01.py
+++ exercicio01.py
@@ -1,2 +1,17 @@
-import biblioteca from Animal
+from biblioteca import Gato, Cachorro, Vaca, Coelho
 
+gato = Gato ("pretin", "preto", "peixe")
+gato.miar()
+gato.comer()
+
+cachorro = Cachorro("caramelo", "caramelo", "ração")
+cachorro.latir()
+cachorro.comer()
+
+vaca = Vaca("grace", "Marron", "grama")
+vaca.mugir()
+vaca.comer()
+
+coelho = Coelho("perna longa", "cinza", "cenoura")
+coelho.pular()
+coelho.comer()
